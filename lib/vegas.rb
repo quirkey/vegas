5f60772384d698dfd52a577980c3d9c7b813eefd
Aaron Quint <aaron@quirkey.com>
2009-06-10 16:53:08 -0400

    Added check for win32, loads the process gem to get 'fork'

--- lib/vegas.rb
+++ lib/vegas.rb
@@ -2,10 +2,19 @@ require 'rubygems'
 require 'sinatra'
 require 'launchy'
 
+# check for windows
+if RUBY_PLATFORM =~ /(mingw|bccwin|wince)/i
+  begin
+    require 'win32/process'
+  rescue 
+    puts "Sorry, in order to use Vegas on Windows you need the win32-process gem:\n gem install win32-process"
+  end
+end
+
 $LOAD_PATH.unshift File.dirname(__FILE__)
 
 module Vegas
-  VERSION = "0.0.1"
+  VERSION = "0.0.2"
   
   autoload :Runner, 'vegas/runner'
 end
