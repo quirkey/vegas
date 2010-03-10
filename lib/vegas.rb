3b8e849559401f8b06cd28a99e0aeac9ddccaff0
Aaron Quint <aaron@quirkey.com>
2010-03-09 23:58:38 -0500

    Version bump

--- lib/vegas.rb
+++ lib/vegas.rb
@@ -8,7 +8,7 @@ end
 $LOAD_PATH.unshift File.dirname(__FILE__)
 
 module Vegas
-  VERSION = "0.1.4"
+  VERSION = "0.1.5"
   WINDOWS = !!(RUBY_PLATFORM =~ /(mingw|bccwin|wince|mswin32)/i)
   
   autoload :Runner, 'vegas/runner'
